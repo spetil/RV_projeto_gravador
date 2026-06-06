f918b35292381941504eaa5541ad5b97f2450fa3
YTAL0 <ytalo122@hotmail.com>
2026-06-05 22:52:20 -0300

    vai agr

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  base: '/gravador/',
+  base: '/RV_projeto_gravador/',
 
   plugins: [
     vue(),
